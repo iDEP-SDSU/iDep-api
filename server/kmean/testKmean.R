537f08ab505338ef756eaa1d28fafa9851a785aa
Eun Woo Son <eunwoo.son@sdstate.edu>
2017-11-03 12:03:55 -0500

    update docker ;9Ccompose

--- server/kmean/testKmean.R
+++ server/kmean/testKmean.R
@@ -1,7 +1,9 @@
 # Test kmean 
 
+source('setup.R', echo=F)
 source('readData.R')
 source('kmean/kmean.R')
+
 
 library(tictoc)
 
@@ -12,8 +14,22 @@ data <- readDataObj$finalResult$rawCounts
 
 # Initialize Kmean 
 kmeanObj <- KmeansDrive$new(data)
+
+kmeanObj$originData
+kmeanObj$heatmapData
+kmeanObj$bar
+
+
+kmeanObj2 <- KmeansDrive$new(data)
+kmeanObj2$originData
+
+
+kmeanObj$myheatmapProcess(n=1000)
+kmeanObj$myHeatMapData
+
 # kmeanObj$heatmapData
 kmeanObj$myheatmap2(n=1000)
+
 toc()
 
 # Test different parameters 
@@ -21,6 +37,8 @@ n=3000
 k=4
 kmeanObj$preprocess(n=n, k=k)
 kmeanObj$myheatmap2(1000)
+
+
 
 # Takes 20s
 # n=4000 
